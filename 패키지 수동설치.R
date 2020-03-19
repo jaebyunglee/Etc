b2762f901e66b4c27726d5acb07ecb15ebb97d64
jaebyunglee <49424681+jaebyunglee@users.noreply.github.com>
2020-03-19 12:18:46 +0900

    Add files via upload

---
+++ 패키지 수동설치.R
@@ -0,0 +1,3 @@
+# 패키지 수동설치하기
+install.packages("C:/Users/begas/Downloads/doParallel_1.0.15.zip", repos = NULL, type = "win.binary")
+install.packages("C:/Users/begas/Downloads/adabag_4.2.zip", repos = NULL, type = "win.binary")

d83dd710a69496829901bfa37057dd4769c25b0d
jaebyunglee <49424681+jaebyunglee@users.noreply.github.com>
2020-03-19 12:20:10 +0900

    Add files via upload

---
+++ 패키지 수동설치.R
@@ -0,0 +1,5 @@
+# 패키지 수동설치하기
+install.packages("C:/Users/begas/Downloads/doParallel_1.0.15.zip", repos = NULL, type = "win.binary")
+install.packages("C:/Users/begas/Downloads/adabag_4.2.zip", repos = NULL, type = "win.binary")
+
+# 설치 후 불러오기가 안되면 Cran 홈페이지에서 Depends에 있는 패키지들 먼저 설치
